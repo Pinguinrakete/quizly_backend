83eb1b9438361ceadac61def089854956b3abd2b
Pinguinrakete <ekirschke@web.de>
2025-10-30 11:12:11 +0100

    add permission IsOwner in quiz_app

---
+++ quiz_app/api/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+    """
+    "Allows access only for the owner of an object."
+    """
+    def has_object_permission(self, request, view, obj):
+
+        return obj.owner == request.user
--- quiz_app/api/views.py
+++ quiz_app/api/views.py
@@ -1,10 +1,19 @@
+from .permissions import IsOwner
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CreateQuizView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     pass
 
 class QuizzesView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     pass
 
 class QuizSingleView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwner]
     pass
