03d1c9c582bf0d15ac9cd942426ed6fef3192705
Pinguinrakete <ekirschke@web.de>
2025-10-30 17:30:59 +0100

    add update model Quiz in models.py in qiuz_app

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Quiz, QuizQuestionDetails, QuestionOptionDetails
 
-# Register your models here.
+admin.site.register(Quiz)
+admin.site.register(QuizQuestionDetails)
+admin.site.register(QuestionOptionDetails)
--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 
+class QuestionOptionDetails(models.Model):
+    option_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.option_text
+    
+
+class QuizQuestionDetails(models.Model):
+    question_title = models.CharField(max_length=255)
+    question_options = models.ManyToManyField(QuestionOptionDetails, blank=True)
+    answer = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.pk and self.question_options.count() != 4:
+            from django.core.exceptions import ValidationError
+            raise ValidationError("Each question must have exactly 4 answer options.")
+
+    def __str__(self):
+        return self.question_title
+    
+
 class Quiz(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
@@ -8,5 +31,5 @@ class Quiz(models.Model):
     video_url = models.CharField(max_length=255)
     questions = models.ManyToManyField(QuizQuestionDetails, blank=True)
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    def __str__(self):
+        return self.title
