ef015d9b5334cde6f51176ed5f117495a2858317
Pinguinrakete <ekirschke@web.de>
2025-11-02 11:26:12 +0100

    Add transcribe_whisper method to utils in quiz_app

--- quiz_app/api/utils.py
+++ quiz_app/api/utils.py
@@ -22,3 +22,18 @@ class AudioQuestionGenerator:
             self.file = "file"
             # self.filename = info['title']
             print(f"✅ Successfully downloaded", self.file)
+
+
+    def transcribe_whisper(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"🚀 Using device: {device}")
+        model = whisper.load_model("small", device=device)  # "tiny = 75MB", "base = 142MB", "small = 466MB", "medium = 1,5GB", "large = 2,9GB"
+        audio_file = f"media/{self.file}.wav"
+
+        result = model.transcribe(audio_file)
+
+        text_file_path = f"media/{self.file}.txt"
+        with open(text_file_path, 'w', encoding='utf-8') as f:
+            f.write(result['text'])
+
+        print(f"✅ Transcription saved in {text_file_path}")
