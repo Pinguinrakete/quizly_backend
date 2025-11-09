7123581925d6c59bb0808b54c6f497d8ad5b3f51
Pinguinrakete <ekirschke@web.de>
2025-11-09 18:48:00 +0100

    Add method delete to QuizSingleView in quiz_app

--- quiz_app/api/views.py
+++ quiz_app/api/views.py
@@ -2,9 +2,10 @@ from .permissions import IsOwner, HeaderOrCookieJWTAuthentication
 from .serializers import YoutubeURLSerializer, CreateQuizSerializer, MyQuizzesSerializer, QuizSinglePatchSerializer
 from .utils import AudioQuestionGenerator
 from django.contrib.auth.models import User
+from django.db import DatabaseError
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.views import APIView
@@ -145,11 +146,11 @@ class QuizSingleView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    def delete(self, request, id):
+    def delete(self, request, pk):
+        quiz = get_object_or_404(Quiz, id=pk)
         try:
-            quiz = Quiz.objects.get(pk=id)
-        except Quiz.DoesNotExist:
-            return Response({"detail": "Quiz not found."}, status=status.HTTP_404_NOT_FOUND)       
+            quiz.delete()
+        except DatabaseError as e:
+            return Response({'error': f'Unexpected error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        quiz.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
