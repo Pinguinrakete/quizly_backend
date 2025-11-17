908340f12b14be25d53e55cb1c99e5befaec0112
Pinguinrakete <ekirschke@web.de>
2025-11-17 12:29:32 +0100

    add CORS_ALLOW_CREDENTIALS and CORS_ALLOW_ALL_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -66,7 +66,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "core.urls"
 
